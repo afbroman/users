b69f3e9784e26c5e2dc1e27eb1bce90b0fa7aae5
Andrew Broman <afbroman@icloud.com>
2018-02-14 16:42:33 -0600

    Implement basic schema; add hardcoded data

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -18,8 +24,12 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
 
-      }
-    }
-  }
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
